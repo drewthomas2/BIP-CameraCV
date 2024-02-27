a6e22a05b464e737fe0ae43537fd37b9cf1e9c3f
drew2thomas <drew.thomas@comcast.net>
2024-02-27 08:27:00 -0800

    changing stuff

---
+++ color-sense.py
@@ -0,0 +1,51 @@
+# BIP C0L0R SENSOR PROTOTYPE
+# DREW THOMAS
+# 1/13/24
+
+from __future__ import print_function
+import numpy as np
+import cv2 as cv
+import time
+from picamera2 import Picamera2
+from picamera.array import PiRGBArray
+import smbus
+
+address = 0x60 # change
+reg_write_dac = 0x40
+bus = smbus.SMBus(1))
+
+n=0
+while n<2:
+    picam = Picamera2()
+    rawCapture = PiRGBArray(picam)
+
+    picam.start()
+    time.sleep(1)
+    img = picam.capture_array("main")
+
+    picam.close()
+
+    # REDUCING IMAGE RESOLUTION 
+    #redu_factor = 1
+    #new_img = cv.resize(img, None, fx = redu_factor, fy = redu_factor, interpolation = cv.INTER_AREA)
+    #height, width = new_img.shape[:2]
+    #print('height: ' + str(height) + ' width: ' + str(width))
+
+    # CONVERT TO HSV
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    h, s, v = cv.split(hsv)
+
+    m_h = np.apply_along_axis(lambda x: np.bincount(x).argmax(), axis=0, arr=h)
+    # m_s = np.apply_along_axis(lambda x: np.bincount(x).argmax(), axis=0, arr=s)
+    # m_v = np.apply_along_axis(lambda x: np.bincount(x).argmax(), axis=0, arr=v)
+    mode_h = 2*m_h[0]
+    #mode_s = m_s[0]
+    #mode_v = m_v[0]
+    s = time.ctime() + ', h: ' + str(mode_h) + ', '
+    print(s)
+    log = open('BIP_LOG.txt', 'a')
+    log.write(s)
+    log.close()
+
+    bus.write_i2c_block_data(address, reg_write_dac, mode_h)
+    n += 1
